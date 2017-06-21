ab19ea0caccc6dee69e5c1fb7e68aaa1db8f72a8
Chavane Minto <chevy@MacBook-Pro-15.dhcp.thefacebook.com>
2017-06-20 17:08:38 -0700

    Calculator with functional Slider. Use of 3 set tip percentages or Slider for Free hand percentage between 12 and 30.

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var percent: UISegmentedControl!
+    
+    @IBOutlet weak var fhpercentage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onScreenTap(_ sender: Any)
+    {
+        view.endEditing(true)
+    }
 
+    @IBAction func calculateTip(_ sender: Any)
+    {
+        let tipPercentage = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0 //change text to double. if user enters non-numerics, default 0
+        let tip = bill * tipPercentage[percent.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    @IBAction func onSliderChange(_ sender: UISlider)
+    {
+        let sliderValue = Double(sender.value)
+        fhpercentage.text = String(format: "%.2f","%", sliderValue)
+        let tipPercentage = sliderValue * 0.01
+        let bill = Double(billField.text!) ?? 0 //change text to double. if user enters non-numerics, default 0
+        let tip = bill * tipPercentage
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
